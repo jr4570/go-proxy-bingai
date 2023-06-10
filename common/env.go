5aed307116445620a80991bb8e73b83746dad467
jr4570 <98681506+jr4570@users.noreply.github.com>
2023-06-10 10:24:29 +0800

    Update env.go

--- common/env.go
+++ common/env.go
@@ -15,7 +15,7 @@ var (
 	// user token
 	USER_TOKEN_ENV_NAME_PREFIX = "Go_Proxy_BingAI_USER_TOKEN"
 	USER_TOKEN_LIST            []string
-	// 访问权限密钥，可选
+	// 訪問權限密鑰，可選
 	AUTH_KEY             string
 	AUTH_KEY_COOKIE_NAME = "BingAI_Auth_Key"
 )
